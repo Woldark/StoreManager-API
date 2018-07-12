62c677ecc2c114d32d462da0ff4c62748065fad1
hatamiarash7 <hatamiarash7@gmail.com>
2018-07-13 01:02:07 +0430

    make fields nullable

--- database/migrations/2018_07_10_111230_create_goods_table.php
+++ database/migrations/2018_07_10_111230_create_goods_table.php
@@ -1,47 +1,47 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateGoodsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('goods', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-            $table->integer('type_id')->unsigned()->index()->nullable();
-            $table->integer('room_id')->unsigned()->index()->nullable();
-            $table->string('name');
-            $table->string('property_number')->unique();
-            $table->string('kind');
-            $table->string('old_property_number')->unique();
-            $table->string('old_kind');
-            $table->text('description');
-            $table->string('model');
-            $table->string('serial');
-            $table->string('barcode');
-            $table->string('status');
-            $table->string('picture');
-            $table->string('unit');
-            $table->foreign('type_id')->references('id')->on('types');
-            $table->foreign('room_id')->references('id')->on('rooms');
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('goods', function (Blueprint $table) {
+			$table->increments('id');
+			$table->timestamps();
+			$table->integer('type_id')->unsigned()->index()->nullable();
+			$table->integer('room_id')->unsigned()->index()->nullable();
+			$table->string('name');
+			$table->string('property_number')->unique();
+			$table->string('kind');
+			$table->string('old_property_number')->unique();
+			$table->string('old_kind');
+			$table->text('description')->nullable();
+			$table->string('model');
+			$table->string('serial');
+			$table->string('barcode');
+			$table->string('status');
+			$table->string('picture')->nullable();
+			$table->string('unit');
+			$table->foreign('type_id')->references('id')->on('types');
+			$table->foreign('room_id')->references('id')->on('rooms');
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('goods');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('goods');
+	}
 }
