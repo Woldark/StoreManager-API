6a5df63ad3dd90e0ab3a6ba0a037fe5027d673b8
hatamiarash7 <hatamiarash7@gmail.com>
2018-07-19 01:57:30 +0430

    add controllers

---
+++ app/Http/Controllers/Admin/RoomController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+	public function index()
+	{
+
+	}
+
+	public function create()
+	{
+
+	}
+
+	public function save(Request $request)
+	{
+
+	}
+
+	public function edit($id)
+	{
+
+	}
+
+	public function update(Request $request)
+	{
+
+	}
+
+	public function delete($id)
+	{
+
+	}
+}
