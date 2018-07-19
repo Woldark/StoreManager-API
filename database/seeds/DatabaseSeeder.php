706f38215f034f44264fea6af4e6985a3c902d16
hatamiarash7 <hatamiarash7@gmail.com>
2018-07-19 23:30:42 +0430

    some changes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,26 +4,24 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-	/**
-	 * Seed the application's database.
-	 *
-	 * @return void
-	 */
+	protected $floors;
+
 	public function run()
 	{
 		factory(\App\Building::class, rand(1, 10))
 			->create()
 			->each(function (\App\Building $building) {
 				$building->floors()->saveMany(
-					factory(\App\Floor::class, rand(5, 15))
+					$this->floors = factory(\App\Floor::class, rand(5, 15))
 						->make(['building_id' => $building->id])
-						->each(function (\App\Floor $floor) {
-							$floor->rooms()->saveMany(
-								factory(\App\Room::class, rand(15, 30))
-									->make(['floor_id' => $floor->id])
-							);
-						})
 				);
 			});
+
+		foreach ($this->floors as $floor) {
+			$floor->rooms()->saveMany(
+				factory(\App\Room::class, rand(15, 30))
+					->make(['floor_id' => $floor->id])
+			);
+		}
 	}
 }
